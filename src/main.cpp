d4028c7da71844e8266f2c360851a3011a1800ec
Zhao <yzhao01@germyzhao.myfiosgateway.com>
2020-12-31 14:59:02 -0500

    final sub

--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,8 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
     float start_x, start_y, end_x, end_y;
-    std::cout << "Please enter the numbers with the following order: `start_x`, `start_y`, `end_x`, and `end_y` " << std::endl;
-    std::cin >> start_x;
+    std::cout << "Please enter the numbers with the following order:(enter key to seperate) `start_x`, `start_y`, `end_x`, and `end_y` " << std::endl;
+    std::cin >> start_x ;
     std::cin >> start_y; 
     std::cin >> end_x;
     std::cin >> end_y;
